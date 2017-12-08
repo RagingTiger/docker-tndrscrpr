a3a4f4d310ca30f59c55d60bf7fd8e710dc5499a
01101010 <jander43@vols.utk.edu>
2017-12-08 13:11:06 -0500

    Fixed tab/spaces issue

--- tinderGetPhotos.py
+++ tinderGetPhotos.py
@@ -2,14 +2,16 @@ import requests
 import json
 import sys
 import urllib.request
-import os, os.path
+import os
+import os.path
 import random
 import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 '''
-For this script to run, the following must be specified, and the user must create an "imagePath" folder for the photos to be saved to.
+For this script to run, the following must be specified, and the user must
+create an "imagePath" folder for the photos to be saved to.
 '''
 
 # Refer to README.md for how to get token
@@ -23,71 +25,76 @@ imagePath = os.environ.get('TINDERPICS_DIR')
 
 
 def waitABit(minTime, maxTime):
-	wait = random.uniform(minTime, maxTime)
-	print("WAIT: " + str(wait) + "\n")
-	time.sleep(wait)
+    wait = random.uniform(minTime, maxTime)
+    print("WAIT: " + str(wait) + "\n")
+    time.sleep(wait)
 
 
 def tinderAPI_get_xAuthToken(facebook_token, facebook_id):
 
-	loginCredentials = {'facebook_token': facebook_token, 'facebook_id': facebook_id}
-	headers = {'Content-Type': 'application/json', 'User-Agent': 'Tinder Android Version 3.2.0'}
+    loginCredentials = {'facebook_token': facebook_token,
+                        'facebook_id': facebook_id}
+    headers = {'Content-Type': 'application/json',
+               'User-Agent': 'Tinder Android Version 3.2.0'}
+    r = requests.post('https://api.gotinder.com/auth',
+                      data=json.dumps(loginCredentials), headers=headers)
+    x_auth_token = r.json()['token']
 
-	r = requests.post('https://api.gotinder.com/auth', data=json.dumps(loginCredentials), headers=headers)
-	x_auth_token = r.json()['token']
-
-	return x_auth_token
+    return x_auth_token
 
 
 def tinderAPI_getSubjectList(x_auth_token):
 
-	# Get a list of subjects
-	headers2 = {'User-Agent': 'Tinder Android Version 3.2.0', 'Content-Type': 'application/json', 'X-Auth-Token': x_auth_token}
-	r2 = requests.get('https://api.gotinder.com/user/recs', headers=headers2)
-	subjects = r2.json()['results']
+    # Get a list of subjects
+    headers2 = {'User-Agent': 'Tinder Android Version 3.2.0',
+                'Content-Type': 'application/json',
+                'X-Auth-Token': x_auth_token}
+    r2 = requests.get('https://api.gotinder.com/user/recs', headers=headers2)
+    subjects = r2.json()['results']
 
-	# Return list of subjects
-	return subjects
+    # Return list of subjects
+    return subjects
 
 
 def tinderAPI_passSubject(subject, x_auth_token):
-	_id = subject['_id']
-	headers3 = {'X-Auth-Token': x_auth_token, 'User-Agent': 'Tinder Android Version 3.2.0'}
-	r3 = requests.get('https://api.gotinder.com/pass/' + _id, headers=headers3)
+    _id = subject['_id']
+    headers3 = {'X-Auth-Token': x_auth_token,
+                'User-Agent': 'Tinder Android Version 3.2.0'}
+    r3 = requests.get('https://api.gotinder.com/pass/' + _id, headers=headers3)
 
 
 def getPics(x_auth_token):
 
-	# Get list of subjects
-	subjects = tinderAPI_getSubjectList(x_auth_token)
+    # Get list of subjects
+    subjects = tinderAPI_getSubjectList(x_auth_token)
 
-	# Get the number of photos in directory
-	processed_numPhotos = len([f for f in os.listdir(imagePath) if os.path.isfile(os.path.join(imagePath, f))])
+    # Get the number of photos in directory
+    processed_numPhotos = len([f for f in os.listdir(imagePath) if os.path.isfile(os.path.join(imagePath, f))])
 
-	# Iterate through list of subjects
-	for subject in subjects:
+    # Iterate through list of subjects
+    for subject in subjects:
 
-		# Get the subject ID
-		sid = subject['_id']
+        # Get the subject ID
+        sid = subject['_id']
 
-		# Gets a list of pictures of the subject
-		pictures = subject['photos']
+        # Gets a list of pictures of the subject
+        pictures = subject['photos']
 
-		# Iterate through and save the pictures of the subject
-		for picIndex in range(len(pictures)):
+        # Iterate through and save the pictures of the subject
+        for picIndex in range(len(pictures)):
 
-			# Get the URL for the largest cropped photo
-			processed_picURL = str(pictures[picIndex]['processedFiles'][0]['url'])
+            # Get the URL for the largest cropped photo
+            processed_picURL = str(pictures[picIndex]['processedFiles'][0]['url'])
 
-			# Get the photo and save
-			urllib.request.urlretrieve(processed_picURL, imagePath + '/' + sid + '_' + str(processed_numPhotos) + '.jpg')
-			processed_numPhotos += 1
+            # Get the photo and save
+            urllib.request.urlretrieve(processed_picURL, imagePath + '/' + sid + '_' + str(processed_numPhotos) + '.jpg')
+            processed_numPhotos += 1
 
-		# Wait some random amount of time and then pass the subject
-		waitABit(0.5, 2.0)
+        # Wait some random amount of time and then pass the subject
+        waitABit(0.5, 2.0)
 
-		# Pass the subject
-		tinderAPI_passSubject(subject, x_auth_token)
+        # Pass the subject
+        tinderAPI_passSubject(subject, x_auth_token)
 
 
 if __name__ == '__main__':
